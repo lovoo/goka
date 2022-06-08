1a22711c33df90918ccf83ee4dd48251189eace6
Norbert Klawikowski <norbert.klawikowski@lovoo.com>
2022-06-08 12:22:28 +0200

    Fix initialization of example 3
    * added file topic.go

---
+++ examples/3-messaging/topic/topic.go
@@ -0,0 +1,33 @@
+package topic
+
+import (
+	"github.com/lovoo/goka"
+	"log"
+)
+
+// EnsureStreamExists is a convenience wrapper for TopicManager.EnsureStreamExists
+func EnsureStreamExists(topic string, brokers []string) {
+	tm := createTopicManager(brokers)
+	err := tm.EnsureStreamExists(topic, 8)
+	if err != nil {
+		log.Printf("Error creating kafka topic %s: %v", topic, err)
+	}
+}
+
+// EnsureStreamExists is a convenience wrapper for TopicManager.EnsureTableExists
+func EnsureTableExists(topic string, brokers []string) {
+	tm := createTopicManager(brokers)
+	err := tm.EnsureTableExists(topic, 8)
+	if err != nil {
+		log.Printf("Error creating kafka topic %s: %v", topic, err)
+	}
+}
+
+func createTopicManager(brokers []string) goka.TopicManager {
+	tmc := goka.NewTopicManagerConfig()
+	tm, err := goka.NewTopicManager(brokers, goka.DefaultConfig(), tmc)
+	if err != nil {
+		log.Fatalf("Error creating topic manager: %v", err)
+	}
+	return tm
+}
